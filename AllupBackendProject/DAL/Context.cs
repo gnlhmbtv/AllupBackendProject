d0aec7c7639b1fb6e65b8d7604ec875720dda113
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-02-02 23:44:50 +0400

    feat: added layout

--- AllupBackendProject/DAL/Context.cs
+++ AllupBackendProject/DAL/Context.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AllupBackendProject.DAL
 {
-    public class Context
+    public class Context:DbContext
     {
+        public Context(DbContextOptions<Context>options):base(options)
+        {
+
+        }
     }
 }
--- AllupBackendProject/Startup.cs
+++ AllupBackendProject/Startup.cs
@@ -1,6 +1,9 @@
+using AllupBackendProject.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +15,20 @@ namespace AllupBackendProject
 {
     public class Startup
     {
+        private readonly IConfiguration _config; 
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddDbContext<Context>(opt =>
+            {
+                opt.UseSqlServer(_config["ConnectionStrings:DefaultConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +39,20 @@ namespace AllupBackendProject
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=home}/{action=Index}/{id?}"
+                    );
             });
         }
     }
